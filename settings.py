276c19cbccac31252a62e0fe124776d62ecbdc52
syn <41833563+Syyyyyn@users.noreply.github.com>
2021-03-28 12:32:27 +0200

    added settings.py

---
+++ settings.py
@@ -0,0 +1,7 @@
+domain = 'exemple.com'
+dns_server = '1.1.1.1'
+dynhost_creds = {
+    'username': 'exemple.com-dynhost',
+    'password': 'P@ssw0rd'
+}
+dns_records = ['exemple.com', 'www.exemple.com', 'mail.exemple.com']
