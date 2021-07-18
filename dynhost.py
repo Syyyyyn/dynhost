ba5239ff22d36ae27011c7cca97a4000e75327f7
syn <41833563+Syyyyyn@users.noreply.github.com>
2021-07-18 14:30:32 +0200

    added error handling

--- dynhost.py
+++ dynhost.py
@@ -1,4 +1,4 @@
-import os, sys, re
+import os, sys
 import settings
 from datetime import datetime
 from nslookup import Nslookup as nslookup
@@ -15,37 +15,52 @@ def log(message, error):
 
 def getIPv4():
     ipv4 = os.popen("curl -s 'https://ip4.seeip.org'").read()
-    if not ipv4: log('[-] ERROR: Unable to reach IPv4 API', 1)
+    if not ipv4: log('[-] ERROR: Unable to reach IPv4 API. Please check your Internet connection or firewall policy.', 1)
     else: return ipv4
 
-# Horodatage de l'opération
+# Date stamping the operation
 timestamp = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
 log('{timestamp} :'.format(timestamp=timestamp), 0)
 
-# Vérification de l'adresse IP publique portée par le serveur
+# Error handling
+if settings.zone == '': log('[-] ERROR: Invalid parameter (zone) in settings.py. Please check your settings and try again.', 1)
+if len(settings.nameservers) == 0: log('[-] WARNING: Invalid parameter (nameserver) in settings.py. Public DNS 1.1.1.1 and 1.0.0.1 will be used as default nameservers.', 0)
+for nameserver in settings.nameservers:
+    if nameserver == '': log('[-] ERROR: Invalid parameter (nameserver) in settings.py. Please check your settings and try again.', 1)
+if settings.dynhost_creds['username'] == '': log('[-] ERROR: Invalid parameter (dynhost_creds:username) in settings.py. Please check your settings and try again.', 1)
+if settings.dynhost_creds['password'] == '': log('[-] ERROR: Invalid parameter (dynhost_creds:password) in settings.py. Please check your settings and try again.', 1)
+if len(settings.dns_records) == 0: log('[-] ERROR: Invalid parameter (dns_records) in settings.py. Please check your settings and try again.', 1)
+for record in settings.dns_records:
+    if record == '': log('[-] ERROR: Invalid parameter (dns_records) in settings.py. Please check your settings and try again.', 1)
+
+# Get server's public IP address
 public_ip = getIPv4()
 log('[+] Public IP address is {public_ip}'.format(public_ip=public_ip), 0)
 
-# Vérification de l'enregistrement DNS
-dns_lookup = nslookup(dns_servers=[settings.dns_server]).dns_lookup(settings.domain).answer[0]
-log('[+] DNS record for {domain} is {ip}'.format(
-    domain = settings.domain,
-    ip = dns_lookup
-), 0)
+# Resolve domain name IP address
+log('[+] Resolving {zone}'.format(zone=settings.zone), 0)
+nslookup_response = None
+if settings.nameservers == '':
+    if not nslookup(dns_servers=['1.1.1.1', '1.0.0.1']).dns_lookup(settings.zone).answer: log('[-] ERROR: Could not resolve {zone} (EMPTY_RESPONSE).'.format(zone=settings.zone), 1)
+    else: nslookup_response = nslookup(dns_servers=['1.1.1.1', '1.0.0.1']).dns_lookup(settings.zone).answer[0]
+else:
+    if not nslookup(dns_servers=settings.nameservers).dns_lookup(settings.zone).answer: log('[-] ERROR: Could not resolve {zone} (EMPTY_RESPONSE).'.format(zone=settings.zone), 1)
+    else: nslookup_response = nslookup(dns_servers=['1.1.1.1', '1.0.0.1']).dns_lookup(settings.zone).answer[0]
+log('[+] Current IP address for {zone} is {ip}'.format(zone=settings.zone, ip=nslookup_response), 0)
 
-# Mise à jour de la zone DNS
-if not public_ip == dns_lookup:
-    for domain in settings.dns_records:
-        log('[+] Updating {domain}'.format(domain=domain), 0)
-        request = 'http://www.ovh.com/nic/update?system=dyndns&hostname={domain}&myip={ip}'.format(
-            domain = domain,
-            ip = public_ip
-        )
-        os.popen('curl -su {username}:{password} "{request}" > /dev/null'.format(
+# Update DNS zone
+if not public_ip == nslookup_response:
+    for record in settings.dns_records:
+        request = 'http://www.ovh.com/nic/update?system=dyndns&hostname={record}&myip={ip}'.format(record=record, ip=public_ip)
+        response = os.popen('curl -su {username}:{password} "{request}"'.format(
             username = settings.dynhost_creds['username'],
             password = settings.dynhost_creds['password'],
             request = request
         )).read()
-else: log('[+] DNS zone for {domain} is up to date'.format(domain=settings.domain), 1)
+        if 'good' in response: log('[+] Updating {record}'.format(record=record), 0)
+        if 'nohost' in response: log('[-] WARNING: No record found for {record}'.format(record=record), 0)
+        if '401 Authorization Required' in response: log('[-] ERROR: Wrong credentials. Please check your settings and try again.'.format(record=record), 1)
+else: log('[+] DNS zone for {zone} is up to date'.format(zone=settings.zone), 1)
 
-log('[+] DNS zone for {domain} was updated'.format(domain=settings.domain), 0)
+# It made it to the end, congrats
+log('[+] DNS zone for {zone} was updated'.format(zone=settings.zone), 0)
